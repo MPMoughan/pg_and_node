4cb96e15634d0d24d557c866df582684284981ef
MPMoughan <MattM@AmandoBs-MacBook-Pro-2.local>
2014-10-18 21:02:57 -0700

    pg and node lab

--- web/app.js
+++ web/app.js
@@ -10,71 +10,76 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
+// empty array for creating instances of library
 var library = new Library();
 
-//Home
+//Home page
 app.get('/', function(req, res){
   res.render('home');
 });
 
 //Index
 app.get('/books', function(req, res){
-  //DONE!
-  console.log("/BOOKS")
-  var leBooks = library.all();
-  res.render('library/index', {allBooks: leBooks});
+  //DONE! Give us all books
+  console.log("/BOOKS");
+  library.all(function(leBooks){
+    res.render('library/index', {allBooks: leBooks});
+  });
 });
+// library.all has found the books and render
+// buzzer help
+
 
 //New
 app.get('/books/new', function(req, res){
-  //DONE
 	res.render("library/new");
 });
 
+
 //Create
 app.post('/books', function(req, res) {
-	//TODO
   console.log("/books -> Implement me.");
-  // library.add ....
-  res.redirect('/books'); 
+    library.add(req.body.book.title, req.body.book.author, function(){
+  res.redirect('/books');
+  });
 });
+
 
 //Show
 app.get('/books/:id', function(req, res) {
   var id = req.params.id;
-  //TODO
-  console.log("/books -> Implement me.");
-  // library.findById ...
-  // Add library/show.ejs page and render it with found book
-  // Add "Show" link on '/books' page.
-  res.send("implement show book. showing book " + req.params.id);
+  library.findById(id, function(foundBook){
+    res.render('library/show', {book:foundBook});
+  });
 });
 
 //Edit
 app.get('/books/:id/edit', function(req, res) {
-	var id = req.params.id;
-  //TODO
-  console.log("/books/:id/edit -> Implement me.");
-  // library.findById ...
-  var foundBook = {};
-  res.render('library/edit', {book: foundBook});
+  var id = req.params.id;
+  library.findById(id, function(foundBook){
+    res.render('library/edit', {book: foundBook});
+  });
 });
 
 //Update
 app.put('/books/:id', function(req, res) {
 	var id = req.params.id;
+  var title = req.body.book.title;
+  var author = req.body.book.author;
   //TODO
-  console.log("/books/:id -> Implement me.");
-  // library.update ...
+  console.log(id,title, author);
+  library.update(id,title, author, function(){
   res.redirect('/books');
+  });
 });
 
 //Delete
 app.delete('/books/:id', function(req, res) {
 	var id = req.params.id;
   //TODO
-  // library.destroy ...
+  library.destroy(id, function(){
   res.redirect('/books');
+  });
 });
 
 var server = app.listen(3000, function() {
--- web/library.js
+++ web/library.js
@@ -5,7 +5,7 @@ var DB = require('./mydb_lib.js');
 var db = new DB("library_example_app", 5432, "localhost");
 
 function Book(title, author, id) {
-  this.id = 0;
+  this.id = id;
   this.title = title;
   this.author = author;
 }
@@ -13,45 +13,54 @@ function Book(title, author, id) {
 function Library() {
 }
 // TOGETHER!
-Library.prototype.all = function() {
+Library.prototype.all = function(buzzer) {
 	var allBooks = [];
 
-	// retrieve books
-	allBooks.push(new Book('The Great Gatsby', 'Fitzgerald', 1));
-	allBooks.push(new Book('Tin Drum', 'Grass', 1));
-
-	return allBooks;
+ 	db.query("SELECT * FROM books;", [], function(err, resultSet){
+  	if (err) console.log("QUERY FAILED", err);
+    resultSet.rows.forEach(function(row){
+    	var aBook = new Book(row.title, row.author, row.id);
+ 			console.log(aBook);
+ 			allBooks.push(aBook);
+    });
+    buzzer(allBooks);
+	});
 };
 
 Library.prototype.add = function(title, author, buzzer) {
-	var newBook = {}
 	// TODO
 	// db.query... INSERT
 	// call buzzer with the new book
-	buzzer(newBook);
+  db.query("INSERT INTO books (title, author) VALUES ($1, $2);", [title, author], function(err, resultSet){
+    if (err) console.log("QUERY FAILED", err);
+
+    buzzer();
+  });
 };
 
 Library.prototype.destroy = function(id, buzzer) {
 	// TODO
-	// db.query... DELETE
-	// call buzzer without params when done
-	buzzer();
+	db.query("DELETE FROM BOOKS WHERE id = $1", [id] , function(err, resultSet){
+	   if (err) console.log("QUERY FAILED", err);
+
+    buzzer();
+  });
 };
 
 Library.prototype.update = function(id, title, author, buzzer) {
-	// TODO
-	// db.query... UPDATE
-	// call buzzer without params when done
-	buzzer();
+  db.query("UPDATE books SET title = $1, author = $2 WHERE id = $3", [title,author,id], function(err,resultSet){
+    if (err) console.log("QUERY FAILED", err);
+    buzzer();
+  });
 };
 
-
 Library.prototype.findById = function(id, buzzer) {
-	var foundBook = {}
-	// TODO
-	// db.query... SELECT
-	// call buzzer with the book found
-	buzzer(foundBook);
+  var foundBook = {}
+  db.query("SELECT * FROM books WHERE id=$1",[id], function(err,resultSet){
+    if(err) console.log("OOPS, something went wrong", err);
+    var foundBook = resultSet.rows[0];
+    buzzer(foundBook);
+  });
 };
 
 module.exports = Library;
